bf75ace879f86c41accbe85d9f199ad59b61eab8
Keerat Sachdeva <keeratsachdeva0@gmail.com>
2021-04-16 13:06:19 +0530

    Initial Commit

---
+++ JS/Script.js
@@ -0,0 +1,18 @@
+function rotateHands(){
+    let date = new Date();
+    
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+
+    let hoursRotation = 30 * h + m / 2;
+    let minutesRotation = 6 * m;
+    let secondsRotation = 6 * s;
+
+    hours.style.transform = `rotate(${hoursRotation}deg)`;
+    minutes.style.transform = `rotate(${minutesRotation}deg)`;
+    seconds.style.transform = `rotate(${secondsRotation}deg)`;
+}
+
+rotateHands();
+setInterval(rotateHands, 1000); // Call rotateHands() after every second.
